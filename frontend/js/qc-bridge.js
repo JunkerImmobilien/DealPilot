c1f9215ed256e1a61e28e5c47d855ce30f1437b4
Marcel Junker <info@junker-immobilien.io>
2026-06-16 06:40:53 +0000

    v688 qb-modal-absview: Modal absolute in sichtbare Content-Mitte

--- frontend/js/qc-bridge.js
+++ frontend/js/qc-bridge.js
@@ -28,7 +28,7 @@
  *        window.qcImportPdfTrigger() → Werte zurück in die iframe-Felder.
  */
 (function () {
-  var IFRAME_SRC = 'quickcheck-app.html?v=687';
+  var IFRAME_SRC = 'quickcheck-app.html?v=688';
 
   // v345: doppelte weiße "⚡ Quick-Check"-Überschrift entfernen.
   // Sie ist ein CSS-Pseudo-Element (body.qc-standalone-active #s-quick::before)
